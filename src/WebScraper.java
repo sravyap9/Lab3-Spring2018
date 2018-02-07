c34305256e102bfc8da7f098ee5b486faf22fddf
sravyapatakota <sravyap2@illinois.edu>
2018-02-07 14:52:10 -0600

    new changes

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,36 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+import java.io.File;
+
 public class WebScraper {
+
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+
+    }
+
+     public int wordCounter(final String word) {
+        return 5;
+    }
+
+
     public static void main(String[] unused) {
-        System.out.println("Ran");
+        System.out.println(urlToString("http://erdani.com/tdpl/hamlet.txt"));
     }
 }
